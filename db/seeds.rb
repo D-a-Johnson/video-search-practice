e930d46f7451117796d1789b11459ad4312e3c70
Derek Johnson <wars2cool@gmail.com>
2021-09-06 05:39:37 +0900

    seed file update to pull from csv and add categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+video_file = 'videos.csv'
+
+puts 'Cleaning database...'
+Movie.destroy_all
+Category.destroy_all
+
+puts 'Creating categories...'
+Category.create!([{ name: 'Mathematik' }, { name: 'Deutsch' }, { name: 'Chemie'}, { name: 'Biologie'}, { name: 'Geschichte' }])
+
+puts 'Creating videos'
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+CSV.foreach(video_file, csv_options) do |row|
+  t = Movie.new
+  t.title = row['title']
+  t.category = Category.find_by_name(row['category'])
+  t.save!
+end
