dac88188509b47646cb03e0a1d6ca0ff49a97c41
Derek Johnson <wars2cool@gmail.com>
2021-09-06 05:53:08 +0900

    Movie model search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ApplicationRecord
   belongs_to :category
+
+  def self.search(search) ##search function that also ignores case
+    if search
+      joins(:category).where("lower(title) LIKE ? OR lower(categories.name) LIKE ?", "%#{search}%", "%#{search}%").order('id DESC')
+    else
+      order('id DESC')
+    end
+  end
 end
